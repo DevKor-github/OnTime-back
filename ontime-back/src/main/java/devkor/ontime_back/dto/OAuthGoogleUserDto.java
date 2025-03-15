18fe13343f822d59f8b04c74c5c22af4742f0266
jjjh02 <jinnyshur@korea.ac.kr>
2025-03-16 00:20:48 +0900

    mod::googleUser에서 받아오는 정보 수정

--- ontime-back/src/main/java/devkor/ontime_back/dto/OAuthGoogleUserDto.java
+++ ontime-back/src/main/java/devkor/ontime_back/dto/OAuthGoogleUserDto.java
@@ -1,21 +1,19 @@
 package devkor.ontime_back.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
 public class OAuthGoogleUserDto {
 
     private String id;           // 고유 사용자 ID
     private String name;          // 사용자 이름
-    @JsonProperty("given_name") // JSON의 given_name 필드와 매핑
-    private String givenName;
-    @JsonProperty("family_name") // JSON의 family_name 필드와 매핑
-    private String familyName;
     private String picture;       // 프로필 이미지 URL
     private String email;         // 이메일
-    @JsonProperty("email_verified")
-    private boolean emailVerified; // 이메일 인증 여부
 
+    public OAuthGoogleUserDto(String id, String name, String picture, String email) {
+        this.id = id;
+        this.name = name;
+        this.picture = picture;
+        this.email = email;
+    }
 }
