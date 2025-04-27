7382cc0b6b9c9fe896677f0b4b075fd247828b19
JunbeomKoreaUniv <ku2soon@korea.ac.kr>
2025-04-27 18:38:49 +0900

    WAS재시작 이후 읽어온 알림정보에서 약속,유저를 조회할 때 LazyInitializationException 페치조인으로 해결

--- ontime-back/src/main/java/devkor/ontime_back/repository/NotificationScheduleRepository.java
+++ ontime-back/src/main/java/devkor/ontime_back/repository/NotificationScheduleRepository.java
@@ -1,4 +1,18 @@
 package devkor.ontime_back.repository;
 
-public class NotificationScheduleRepository {
+import devkor.ontime_back.entity.NotificationSchedule;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public interface NotificationScheduleRepository extends JpaRepository<NotificationSchedule, Long> {
+    @Query("SELECT n FROM NotificationSchedule n " +
+            "JOIN FETCH n.schedule s " +
+            "JOIN FETCH s.user " +
+            "WHERE n.notificationTime > :now AND n.isSent = false")
+    List<NotificationSchedule> findAllWithScheduleAndUser(LocalDateTime now);
 }
