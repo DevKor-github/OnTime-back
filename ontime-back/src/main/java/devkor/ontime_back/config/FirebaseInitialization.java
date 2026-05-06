f4b0ceb9cf565317a1635bcfda29d53ed3f8b178
jjoonleo <jjoonleo@gmail.com>
2026-05-06 14:16:23 +0900

    Fix Firebase initialization lifecycle hook

--- ontime-back/src/main/java/devkor/ontime_back/config/FirebaseInitialization.java
+++ ontime-back/src/main/java/devkor/ontime_back/config/FirebaseInitialization.java
@@ -7,7 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
+import jakarta.annotation.PostConstruct;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -31,6 +31,9 @@ public class FirebaseInitialization {
 
             if (FirebaseApp.getApps().isEmpty()) {
                 FirebaseApp.initializeApp(options);
+                log.info("Firebase initialized successfully");
+            } else {
+                log.info("Firebase already initialized");
             }
         } catch (IOException e) {
             log.error("Failed to initialize Firebase", e);
